6f82ac59eb265b4584580939f39594aad5f6c6d3
悠一 <382308351@qq.com>
2017-07-16 22:14:01 +0800

    coding one hour every day.

---
+++ app/src/main/java/com/kami/pcfileexplorer/ui/file/FileContract.java
@@ -0,0 +1,19 @@
+package com.kami.pcfileexplorer.ui.file;
+
+import com.kami.pcfileexplorer.BasePresenter;
+import com.kami.pcfileexplorer.BaseView;
+
+/**
+ * author: youyi_sizuru
+ * data: 2017/7/16
+ */
+
+interface FileContract {
+    interface View extends BaseView<Presenter> {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+}
---
+++ app/src/main/java/com/kami/pcfileexplorer/ui/file/FilePresenter.java
@@ -0,0 +1,23 @@
+package com.kami.pcfileexplorer.ui.file;
+
+/**
+ * author: youyi_sizuru
+ * data: 2017/7/16
+ */
+
+class FilePresenter implements FileContract.Presenter {
+    private FileContract.View mView;
+    FilePresenter(FileContract.View view) {
+        mView = view;
+    }
+
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unSubscribe() {
+
+    }
+}
